21633aff2e8c62524a4084a4a45ff271bdaaa841
Musoye <oyebamijimustapha44@gmail.com>
2023-07-26 16:07:45 +0100

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Base cache algorithm"""
+
+
+from typing import Union
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """A class for FIFO caching"""
+
+    def __init__(self):
+        """Initialization function"""
+        super().__init__()
+
+    def put(self, key, item) -> None:
+        """set a caching value"""
+        if key is not None and item is not None:
+            if len(self.cache_data) > (BaseCaching.MAX_ITEMS - 1):
+                self.cache_data[key] = item
+                first_key = list(self.cache_data.keys())[0]
+                self.cache_data.pop(first_key)
+                print("DISCARD: {}".format(first_key))
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
